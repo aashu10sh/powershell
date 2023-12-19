29ddc22cad5ef602eec267fa0ed54d2e00b23d2e
Aashutosh Pudasaini <nepalidude3@gmail.com>
2023-12-19 16:35:23 +0545

    download method new

--- download_and_install.ps1
+++ download_and_install.ps1
@@ -1,14 +1,11 @@
 Set-Location -Path $ENV:Temp -PassThru
 
-
 $url = "https://dbeaver.io/files/dbeaver-ce-latest-x86_64-setup.exe"
+
 $temp_location = $ENV:Temp;
 
-$WebClient = New-Object System.Net.WebClient
-$WebClient.DownloadFile($url,$temp_location)
 $binary = $url.Split('/')[-1]
-
+Invoke-WebRequest $url  -OutFile $temp_location + $binary
 
 $command = $temp_location +  $binary
-
 Invoke-Expression -Command $command
