9fea5b191ce60d52de6e2be861b53ed20894acaf
Aashutosh Pudasaini <nepalidude3@gmail.com>
2023-12-19 16:39:41 +0545

    download and install fix

--- download_and_install.ps1
+++ download_and_install.ps1
@@ -5,7 +5,8 @@ $url = "https://dbeaver.io/files/dbeaver-ce-latest-x86_64-setup.exe"
 $temp_location = $ENV:Temp;
 
 $binary = $url.Split('/')[-1]
-Invoke-WebRequest $url  -OutFile $temp_location + $binary
+$download_location = $temp_location + $binary 
+Invoke-WebRequest $url  -OutFile $download_location
 
 $command = $temp_location +  $binary
 Invoke-Expression -Command $command
