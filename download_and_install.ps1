9d5c23f59a38368a9c5f8223cdfffb00975cb590
Aashutosh Pudasaini <nepalidude3@gmail.com>
2023-12-19 16:56:53 +0545

    download and installer installer of dbeaver

--- download_and_install.ps1
+++ download_and_install.ps1
@@ -6,7 +6,17 @@ $temp_location = $ENV:Temp;
 
 $binary = $url.Split('/')[-1]
 $download_location = $temp_location + $binary 
-Invoke-WebRequest $url  -OutFile $download_location
+Write-Output "Getting Binary From $url"
+Write-Output "Binary : $binary"
+Write-Output "Download LOcation : $download_location"
+
+Write-Output "Downloading"
+curl $url -o $download_location
+# Invoke-WebRequest $url  -OutFile $download_location
+# wget $url $download_location
+
 
 $command = $temp_location +  $binary
+Write-Output "Executing : $command"
+
 Invoke-Expression -Command $command
